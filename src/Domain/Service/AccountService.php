f24635ff26bd1d94e90e24fd2c1b1a48aeea21dc
CorentinCrz <ccroiz@live.fr>
2022-04-08 15:59:35 +0200

    feat: add unit tests for application service

--- src/Domain/Service/AccountService.php
+++ src/Domain/Service/AccountService.php
@@ -6,7 +6,7 @@ use App\Entity\Account;
 
 class AccountService
 {
-    public function updateTranslation(Account $translation, array $countries, string $company): Account
+    public function updateTranslation(Account $translation, array $countries, ?string $company): Account
     {
         return $translation->setCountries($countries)->setCompany($company);
     }
