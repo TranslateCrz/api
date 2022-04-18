206ab9ed4d0b06e044062ca318250d2ed21b3e74
CorentinCrz <ccroiz@live.fr>
2022-04-18 20:24:02 +0200

    test: api testing for translation controller

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Account;
+use App\Entity\Translation;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -14,12 +15,25 @@ class AppFixtures extends Fixture
             'company' => 'Hetic',
             'countries' => ['fr', 'en', 'es'],
             'roles' => ['ROLE_USER', 'ROLE_ADMIN'],
+            'translations' => [
+                [
+                    'code' => 'home.title',
+                    'country' => 'fr',
+                    'value' => 'Le titre de la page d\'accueil',
+                ],
+                [
+                    'code' => 'home.main.text',
+                    'country' => 'fr',
+                    'value' => 'Bla bla bla ...',
+                ],
+            ],
         ],
         [
             'email' => 'corentin.croizat@hetic.net',
             'company' => null,
             'countries' => ['fr', 'en', 'es'],
             'roles' => ['ROLE_USER'],
+            'translations' => [],
         ],
     ];
 
@@ -38,8 +52,18 @@ class AppFixtures extends Fixture
                 ->setRoles($data['roles'])
             ;
             $manager->persist($account);
+            $this->loadTranslations($manager, $account, $data['translations']);
         }
 
         $manager->flush();
+    }
+
+    public function loadTranslations(ObjectManager $manager, Account $account, array $translations): void
+    {
+        foreach ($translations as $data) {
+            $translation = new Translation($account, $data['code'], $data['country']);
+            $translation->setValue($data['value']);
+            $manager->persist($translation);
+        }
     }
 }
--- tests/Functional/ControllerTestCase.php
+++ tests/Functional/ControllerTestCase.php
@@ -5,6 +5,7 @@ namespace App\Tests\Functional;
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
 use App\Application\Repository\AccountRepositoryInterface;
+use App\Application\Repository\TranslationRepositoryInterface;
 use App\DataFixtures\AppFixtures;
 
 abstract class ControllerTestCase extends ApiTestCase
@@ -33,5 +34,24 @@ abstract class ControllerTestCase extends ApiTestCase
         $account = $repository->findByEmail($email);
 
         return $account?->getUuid();
+    }
+
+    protected function getTranslationIdByCodeAndEmail(
+        string $code = AppFixtures::ACCOUNTS[0]['translations'][0]['code'],
+        string $email = AppFixtures::ACCOUNTS[0]['email'],
+    ): string {
+        $repository = static::getContainer()->get(TranslationRepositoryInterface::class);
+
+        return $repository->createQueryBuilder('t')
+            ->select('t.uuid')
+            ->leftJoin('t.account', 'a')
+            ->andWhere('a.email = :val')
+            ->setParameter('val', $email)
+            ->andWhere('t.code = :code')
+            ->setParameter('code', $code)
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
     }
 }
